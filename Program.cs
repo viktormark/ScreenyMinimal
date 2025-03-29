1c7c0474d437df28ad70799fb4e8e8f90d9e9b26
unknown <mvb9797@gmail.com>
2025-03-29 21:17:00 +0200

    final

--- Program.cs
+++ Program.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using WindowsFormsApp1;
 
-namespace WindowsFormsApp1
+namespace ScreenyMinimal
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            try
+            {
+                // Ваш код, який працює з файлами
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"Файл не знайдено: {ex.FileName}\n{ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Виникла помилка: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
